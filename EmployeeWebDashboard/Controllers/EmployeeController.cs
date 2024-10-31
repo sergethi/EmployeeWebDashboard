060d5ed6d4076e6a95f2882c9581050927fe47c5
sergethi <nsergearistide@gmail.com>
2024-10-31 19:28:28 -0400

    Add project files.

---
+++ EmployeeWebDashboard/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace EmployeeWebDashboard.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET /Employee
+        public IActionResult Index()
+        {
+            return Content("Employee controller");
+        }
+
+        // GET /Employee/Welcome
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}! Welcome  to the Employee dashboard!!... ID: {ID}");
+        }
+    }
+}
---
+++ EmployeeWebDashboard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmployeeWebDashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
