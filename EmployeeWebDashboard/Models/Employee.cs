b7b1996df8572808398b4e6e3e27449fcdb072ec
sergethi <nsergearistide@gmail.com>
2024-11-21 16:50:40 -0500

    updated Employees model

--- EmployeeWebDashboard/Models/Employee.cs
+++ EmployeeWebDashboard/Models/Employee.cs
@@ -7,11 +7,14 @@ namespace EmployeeWebDashboard.Models
     {
         public int Id { get; set; }
         [Display(Name = "Full Name")]
+        [Required]
         public string? FullName { get; set; }
+        [Required]
         public string? Title { get; set; }
         [Display(Name = "Start Date")]
         [DataType(DataType.Date)]
         public DateTime StartDate { get; set; }
+        [Required]
         public string? Team { get; set; }
         public string? Location { get; set; }
         [Display(Name = "Phone Number")]
